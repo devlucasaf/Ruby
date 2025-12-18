2536ecc7add81471ebee3171e80ed0917b0e0682
Lucas Freitas <freitas.lucasaf@gmail.com>
2025-12-18 14:15:23 -0300

    Update cliente_banco.rb

--- cliente_banco.rb
+++ cliente_banco.rb
@@ -1,226 +1,245 @@
 class ClienteBanco
 
-  @@no_of_cliente = 0
-  def  initialize(id, nome, data_nascimento, cpf, nacionalidade, genero, estado_civil, numero, email, profissao, salario, senha)
-    @cliente_id = id
-    @cliente_nome = nome
-    @cliente_data_nascimento = data_nascimento
-    @cliente_nacionalidade = nacionalidade
-    @cliente_genero = genero
-    @cliente_estado_civil = estado_civil
-    @cliente_email = email
-    @cliente_profissao = profissao
-    @cliente_salario = salario
+    @@no_of_cliente = 0
+    def  initialize(id, nome, data_nascimento, cpf, nacionalidade, genero, estado_civil, numero, email, profissao, salario, senha)
+        @cliente_id = id
+        @cliente_nome = nome
+        @cliente_data_nascimento = data_nascimento
+        @cliente_cpf = cpf
+        @cliente_nacionalidade = nacionalidade
+        @cliente_genero = genero
+        @cliente_estado_civil = estado_civil
+        @cliente_numero = numero
+        @cliente_email = email
+        @cliente_profissao = profissao
+        @cliente_salario = salario
+        @cliente_senha = senha
 
-    self.numero = numero
-    self.cpf = cpf
-    self.senha = senha
+        @@no_of_cliente += 1
+    end
 
-    @@no_of_cliente += 1
-  end
-
-  # Método Get
+    # Método Get
 
 =begin
-  Para a criação do método Get e Set, podemos criar-la de duas formas:
+    Para a criação do método Get e Set, podemos criar-la de duas formas:
     O 'attr_accessor' cria automaticamente os métodos Get e Set para você
     attr_accessor :nome, :cpf, :numero, :senha
-  A segunda forma é da forma não comentada no código
+    A segunda forma é da forma não comentada no código
 =end
 
-  def id
-    @cliente_id
-  end
-
-  def nome
-    @cliente_nome
-  end
-
-  def data_nascimento
-    @cliente_data_nascimento
-  end
-
-  def cpf
-    "***********"
-  end
-
-  def nacionalidade
-    @cliente_nacionalidade
-  end
-
-  def genero
-    @cliente_genero
-  end
-
-  def estado_civil
-    @cliente_estado_civil
-  end
-
-  def numero
-    @cliente_numero
-  end
-
-  def email
-    @cliente_email
-  end
-
-  def profissao
-    @cliente_profissao
-  end
-
-  def salario
-    @cliente_salario
-  end
-
-  def senha
-    "******"
-  end
-
-# +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-
-  # Método Set
-
-  def id=(id)
-    @cliente_id = id
-  end
-
-  def nome=(nome)
-    @cliente_nome = nome
-  end
-
-  def data_nascimento=(data_nascimento)
-    @cliente_data_nascimento = data_nascimento
-  end
-
-  def cpf=(valor)
-    if valor.to_s.strip.length != 11
-      raise ArgumentError, "O CPF precisa conter 11 caracteres"
-    else
-      @cliente_cpf = valor
+    def id
+        @cliente_id
     end
-  end
 
-  def nacionalidade=(nacionalidade)
-    @cliente_nacionalidade = nacionalidade
-  end
-
-  def genero=(valor)
-    @cliente_genero = valor
-  end
-
-  def estado_civil=(estado_civil)
-    @cliente_estado_civil = estado_civil
-  end
-
-  def numero=(valor)
-    if valor.to_s.strip.length != 11
-      raise ArgumentError, "O número celular precisa conter 11 números! (DDD + Número)"
-    else
-      @cliente_numero = valor
+    def nome
+        @cliente_nome
     end
-  end
 
-  def numero=(novo_numero)
-    # Remove tudo que não é número (pontos, traços, parênteses)
-    numero_limpo = novo_numero.to_s.gsub(/\D/, "")
-
-    if numero_limpo.length != 11
-      raise ArgumentError, "O número precisa de 11 dígitos (DDD + Número)"
-    else
-      @cliente_numero = numero_limpo
+    def data_nascimento
+        @cliente_data_nascimento
     end
-  end
 
-  def email=(email)
-    @cliente_email = email
-  end
-
-  def profissao=(profissao)
-    @cliente_profissao = profissao
-  end
-
-  def salario=(salario)
-    @cliente_salario = salario
-  end
-
-  def senha=(nova_senha)
-    if nova_senha.length < 6
-      puts "Erro: A senha deve ter no mínimo 6 caracteres!"
-    else
-      @senha = nova_senha
-      puts "Senha alterada com sucesso!"
+    def cpf
+        "***********"
     end
-  end
 
-  def self.total_clientes
-    @@no_of_cliente
-  end
+    def nacionalidade
+        @cliente_nacionalidade
+    end
 
-  def mostrar_dados
-    puts "+=+= " + ("+=+="*69) + "\n"
-    puts "ID: #{@cliente_id}"
-    puts "Nome: #{@cliente_nome}"
-    puts "Data de Nascimento: #{@cliente_data_nascimento}"
-    puts "CPF: #{@cliente_cpf}"
-    puts "Nacionalidade: #{@cliente_nacionalidade}"
-    puts "Gênero: #{@cliente_genero}"
-    puts "Estado Cívil: #{@cliente_estado_civil}"
-    puts "Número: #{@cliente_numero}"
-    puts "E-mail: #{@cliente_email}"
-    puts "Profissao: #{@cliente_profissao}"
-    puts "Salario: R$ #{@cliente_salario}"
-    puts "Senha: #{@senha}\n"
-    puts "+=+= " + ("+=+="*69) + "\n"
-  end
+    def genero
+        @cliente_genero
+    end
+
+    def estado_civil
+        @cliente_estado_civil
+    end
+
+    def numero
+        @cliente_numero
+    end
+
+    def email
+        @cliente_email
+    end
+
+    def profissao
+        @cliente_profissao
+    end
+
+    def salario
+        @cliente_salario
+    end
+
+    def senha
+        "******"
+    end
+
+    # +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+
+    # Método Set
+
+    def id=(id)
+        @cliente_id = id
+    end
+
+    def nome=(nome)
+        @cliente_nome = nome
+    end
+
+    def data_nascimento=(data_nascimento)
+        @cliente_data_nascimento = data_nascimento
+    end
+
+    def cpf=(valor)
+        if valor.to_s.strip.length != 11
+        raise ArgumentError, "O CPF precisa conter 11 caracteres"
+        else
+        @cliente_cpf = valor
+        end
+    end
+
+    def nacionalidade=(nacionalidade)
+        @cliente_nacionalidade = nacionalidade
+    end
+
+    def genero=(valor)
+        @cliente_genero = valor
+    end
+
+    def estado_civil=(estado_civil)
+        @cliente_estado_civil = estado_civil
+    end
+
+    def alterar_numero(numero_atual, novo_numero)
+        numero_atual_limpo = numero_atual.to_s.gsub(/\D/, "")
+        novo_numero_limpo = novo_numero.to_s.gsub(/\D/, "")
+        
+        if numero_atual_limpo != @cliente_numero
+            puts "Erro: número atual incorreto!"
+            return
+        end
+        
+        if novo_numero_limpo.length != 11
+            raise ArgumentError, "O número precisa conter 11 dígitos (DDD + Número)!"
+            return
+        else
+            @cliente_numero = novo_numero_limpo
+            puts "Número atualizado com sucesso!"
+        end
+    end
+
+    def email=(email)
+        @cliente_email = email
+    end
+
+    def profissao=(profissao)
+        @cliente_profissao = profissao
+    end
+
+    def salario=(salario)
+        @cliente_salario = salario
+    end
+
+    def alterar_senha(senha_atual, nova_senha)
+        if senha_atual != @senha
+            puts "Erro: senha atual incorreta!"
+            return
+        end
+        
+        if nova_senha.length < 6
+            raise ArgumentError, "A senha precisa conter no mínimo 6 caracteres!"
+            return
+        else
+            @senha = nova_senha
+            puts "Senha atualizada com sucesso!"
+        end
+    end
+    
+    def self.total_clientes
+        @@no_of_cliente
+    end
+
+    def mostrar_dados
+        puts "+=+= " + ("+=+="*69) + "\n"
+        puts "ID: #{@cliente_id}"
+        puts "Nome: #{@cliente_nome}"
+        puts "Data de Nascimento: #{@cliente_data_nascimento}"
+        puts "CPF: #{@cliente_cpf}"
+        puts "Nacionalidade: #{@cliente_nacionalidade}"
+        puts "Gênero: #{@cliente_genero}"
+        puts "Estado Cívil: #{@cliente_estado_civil}"
+        puts "Número: #{@cliente_numero}"
+        puts "E-mail: #{@cliente_email}"
+        puts "Profissao: #{@cliente_profissao}"
+        puts "Salario: R$ #{@cliente_salario}"
+        puts "Senha: #{@senha}\n"
+        puts "+=+= " + ("+=+="*69) + "\n"
+    end
 end
 
 def main()
-  cliente1 = ClienteBanco.new("1",
-                              "Jonas",
-                              "27/02/2001",
-                              "945.254.121-56",
-                              "Brasileira",
-                              "Masculina",
-                              "Casado",
-                              61994583652,
-                              "jonas.dev@email.com",
-                              "Desenvolvedor",
-                              "15674,39",
-                              "amoeba123")
-  cliente2 = ClienteBanco.new("2",
-                              "Karla",
-                              "25/09/1986",
-                              "123.456.781-36",
-                              "Portuguesa",
-                              "Feminino",
-                              "Solteira",
-                              61999999999,
-                              "karla.maneira301@email.com",
-                              "Juíza",
-                              "37896,45",
-                              "amendoboboyeye0192")
-  cliente3 = ClienteBanco.new("3",
-                              "Leanderson",
-                              "19/03/1875",
-                              "987.654.328-45",
-                              "Brasileira",
-                              "Masculina",
-                              "Viúvo",
-                              24988888888,
-                              "leandersongames5kill@email.com",
-                              "Gamer",
-                              "8932,67",
-                              "6titulos")
+    cliente1 = ClienteBanco.new("1",
+                                "Jonas",
+                                "27/02/2001",
+                                94525412156,
+                                "Brasileira",
+                                "Masculina",
+                                "Casado",
+                                61994583652,
+                                "jonas.dev@email.com",
+                                "Desenvolvedor",
+                                15674.39,
+                                "amoeba123")
+    cliente2 = ClienteBanco.new("2",
+                                "Karla",
+                                "25/09/1986",
+                                12345678136,
+                                "Portuguesa",
+                                "Feminino",
+                                "Solteira",
+                                61999999999,
+                                "karla.maneira301@email.com",
+                                "Juíza",
+                                37896.45,
+                                "amendoboboyeye0192")
+    cliente3 = ClienteBanco.new("3",
+                                "Leanderson",
+                                "19/03/1875",
+                                98765432845,
+                                "Brasileira",
+                                "Masculina",
+                                "Viúvo",
+                                24988888888,
+                                "leandersongames5kill@email.com",
+                                "Gamer",
+                                8932.67,
+                                "6titulos")
+    cliente4 = ClienteBanco.new("4",
+                                "Samuel",
+                                "17/12/2004",
+                                82165274869,
+                                "Brasileira",
+                                "Masculina",
+                                "Solteiro",
+                                24977777777,
+                                "samuelino@email.com",
+                                "Futebolista",
+                                543000.45,
+                                "lobisomenpidao1234")
+    cliente1.mostrar_dados
+    cliente2.mostrar_dados
+    cliente3.mostrar_dados
+    cliente4.mostrar_dados
 
-  cliente1.mostrar_dados
-  cliente2.mostrar_dados
-  cliente3.mostrar_dados
+    puts "Tentando alterar senha com senha correta:"
+    cliente1.alterar_senha("amoeba123", "@Jonaspapakill123")
 
-  puts "Tentando alterar senha com senha correta:"
-  cliente1.senha("amoeba123", "@Jonaspapakill123")
-
-  puts "\nDados atualizados com sucesso!"
-  cliente1.mostrar_dados
+    puts "\nDados atualizados com sucesso!"
+    cliente1.mostrar_dados
+    
+    puts "Tentando alterar o número com o número correto:"
+    cliente4.alterar_numero(24977777777, 24955555555)
 end
 
 main()
